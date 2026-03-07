9b0dfb16d850dcfa0b824382f20f74ac6269a72e
Hao Truong <haotv03@gmail.com>
2026-03-07 21:37:48 +0700

    fix admin token

--- src/auth/context/jwt/auth-provider.tsx
+++ src/auth/context/jwt/auth-provider.tsx
@@ -66,7 +66,7 @@ export function AuthProvider({ children }: Props) {
 
   const initialize = useCallback(async () => {
     try {
-      const accessToken = sessionStorage.getItem(STORAGE_KEY);
+      const accessToken = localStorage.getItem(STORAGE_KEY);
       if (!accessToken || !isValidToken(accessToken)) {
         setSession(null);
         dispatch({ type: Types.INITIAL, payload: { user: null } });
--- src/auth/context/jwt/utils.ts
+++ src/auth/context/jwt/utils.ts
@@ -51,7 +51,7 @@ export const tokenExpired = (exp: number) => {
 
     enqueueSnackbar('Phiên đăng nhập đã hết hạn. Vui lòng đăng nhập lại.', { variant: 'warning' });
 
-    sessionStorage.removeItem('accessToken');
+    localStorage.removeItem('accessToken');
 
     window.location.href = paths.auth.jwt.login;
   }, timeLeft);
@@ -61,7 +61,7 @@ export const tokenExpired = (exp: number) => {
 
 export const setSession = (accessToken: string | null) => {
   if (accessToken) {
-    sessionStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('accessToken', accessToken);
 
     axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
 
@@ -69,7 +69,7 @@ export const setSession = (accessToken: string | null) => {
     const { exp } = jwtDecode(accessToken); // ~3 days by minimals server
     tokenExpired(exp);
   } else {
-    sessionStorage.removeItem('accessToken');
+    localStorage.removeItem('accessToken');
 
     delete axios.defaults.headers.common.Authorization;
   }
